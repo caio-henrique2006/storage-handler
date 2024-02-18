e0f988c390360c6dceca8c410b128272385fd825
Caio Henrique <Caio_Henrique2006@outlook.com>
2024-02-18 09:43:17 -0300

    Complete header

--- storage-handler/src/components/Add.jsx
+++ storage-handler/src/components/Add.jsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 export default function Add () {
     return (
         <div className="Add">
-            <button className="Add_Button">+</button>
+            <div className="Add_icon">
+            </div>
         </div>
     ) 
 }
--- storage-handler/src/components/SearchBar.jsx
+++ storage-handler/src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ export default function SearchBar () {
     return (
         <div className="SearchBar">
             <div className="SearchBar_container">
-                <input className="SearchBar_container_input" type="text" placeholder="Search.." name="search" />
+                <input className="SearchBar_container_input" type="text" placeholder="Procurar..." name="search" />
             </div>
         </div>
     )
