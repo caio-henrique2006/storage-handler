ad91f3226d399054cf41dfd4d4f4296dcb51cfd5
Caio Henrique <Caio_Henrique2006@outlook.com>
2024-02-21 08:37:12 -0300

    add modal

--- storage-handler/src/main.js
+++ storage-handler/src/main.js
@@ -15,8 +15,19 @@ const createWindow = () => {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
   });
+  const window_modal_add = new BrowserWindow({ 
+    parent: mainWindow,
+    modal: true,
+    show: false,
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+    },
+  })
   mainWindow.maximize()
   mainWindow.show()
+  window_modal_add.show()
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
@@ -25,10 +36,14 @@ const createWindow = () => {
   mainWindow.webContents.openDevTools();
 };
 
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
+
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
