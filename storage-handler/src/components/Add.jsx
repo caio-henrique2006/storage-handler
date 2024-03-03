5ce8910768737b83e812fce39a106ffc444ca3ac
Caio Henrique <Caio_Henrique2006@outlook.com>
2024-03-03 09:51:35 -0300

    add modal finished

--- storage-handler/src/components/Add.jsx
+++ storage-handler/src/components/Add.jsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
 
-// components:
-import Modal from "./Modal.jsx";
-
 // const { ipcRenderer } = window.require('electron');
 
 export default function Add () {
@@ -14,13 +11,37 @@ export default function Add () {
         setIsOpen(!isOpen)
     }
 
+    function Modal () {
+        return (
+            <div className="Modal">
+                <h1>Novo Produto</h1>
+                <div className="Modal_grid">
+                    <div className="Modal_left">
+                        <p>Nome do produto:</p>
+                        <input />
+                        <p>Estoque:</p>
+                        <input />
+                        <p>Preço:</p>
+                        <input />
+                    </div>
+                    <div className="Modal_right">
+                        <p>Descrição</p>
+                        <textarea className="Modal_input_description" />
+                    </div>
+                </div>
+                <div className="Modal_close" onClick={openClose}/>
+                <div className="Modal_ok">Criar</div>
+            </div>
+        )
+    }
+
     return (
-        <div className="Add" onClick={openClose}>
-            <div className="Add_icon">
+        <div className="Add">
+            <div className="Add_icon" onClick={openClose}>
             </div>
             <div className="Add_Modal">
                 {
-                    isOpen ? <Modal/> : null
+                    isOpen ? <Modal isOpen={isOpen}/> : null
                 }
             </div>
         </div>
