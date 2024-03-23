d6d852e9f5a8fa93e5ce60b5dd3eb16b7e6e1199
Caio Henrique <Caio_Henrique2006@outlook.com>
2024-03-23 09:29:06 -0300

    Implementing sqlite3 (path problem resolved)

--- storage-handler/src/app.jsx
+++ storage-handler/src/app.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import slash from 'slash';
 import "./style/Main.css"
 import { createRoot } from 'react-dom/client';
 
@@ -8,11 +9,42 @@ import Card from "./components/Card.jsx";
 import Properties from "./components/Properties.jsx"
 
 async function getData(callback) {
-    
+    const sqlite3 = require('sqlite3').verbose();
+    const path = require('path');
+    const dbPath = slash(path.resolve('src/database/chinook.db'));
+    console.log(dbPath);
+
+    let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("Connected to the sqlite data");
+    });
+
+    let sql = `
+    SELECT 
+        title
+    FROM
+        albums
+    `;
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        callback(rows);
+    });
+
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("Close database connection");
+    });
 }
+
 function showData(data) {
     console.log(data);
-    const sqlite3 = require('sqlite3').verbose();
 }
 
 function Main () {
